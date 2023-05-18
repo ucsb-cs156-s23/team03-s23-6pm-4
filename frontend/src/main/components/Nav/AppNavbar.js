0eb4159ad47c16643be4ec773c32b8012ed9beeb
ykeung <pkeung@ucsb.edu>
2023-05-18 14:17:10 -0700

    yk - addded restaurant utils test, modified AppNavbar.js and test

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -81,7 +81,14 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
             <Nav className="mr-auto">
               {
                 hasRole(currentUser, "ROLE_USER") && (
-                  <Nav.Link as={Link} to="/restaurants">Restaurants</Nav.Link>
+                  <NavDropdown title="Restaurants" id="appnavbar-restaurants-dropdown" data-testid="appnavbar-restaurants-dropdown" >
+                    <NavDropdown.Item href="/restaurants/list" data-testid="appnavbar-restaurants-list">List</NavDropdown.Item>
+                    {
+                      hasRole(currentUser, "ROLE_ADMIN") && (
+                        <NavDropdown.Item href="/restaurants/create" data-testid="appnavbar-restaurants-create">Create</NavDropdown.Item>
+                      )
+                    }
+                  </NavDropdown>
                 )
               }
             </Nav>

260b389562acb705a62cfa82e9c15de6ca8aadf7
jessica-lieu <jessicalieu@ucsb.edu>
2023-05-18 15:03:29 -0700

    jl-added mock api calls

--- frontend/src/tests/pages/Apartments/ApartmentDetails.test.js
+++ frontend/src/tests/pages/Apartments/ApartmentDetails.test.js
@@ -3,6 +3,11 @@ import ApartmentDetailsPage from "main/pages/Apartments/ApartmentDetailsPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
+import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+
 const mockNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
@@ -34,6 +39,10 @@ jest.mock('main/utils/apartmentUtils', () => {
 });
 
 describe("ApartmentDetailsPage tests", () => {
+
+    const axiosMock =new AxiosMockAdapter(axios);
+    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither); 
 
     const queryClient = new QueryClient();
     test("renders without crashing", () => {

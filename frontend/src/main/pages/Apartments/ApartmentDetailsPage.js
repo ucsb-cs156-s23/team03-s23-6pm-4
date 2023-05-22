8b22abd95a2dfbd58f4b5fb14afde0a8bfe2c324
Jacob Woodward <jacobwoodward@ucsb.edu>
2023-05-22 14:35:30 -0700

    jw - update ApartmentDetailsPage to use backend

--- frontend/src/main/pages/Apartments/ApartmentDetailsPage.js
+++ frontend/src/main/pages/Apartments/ApartmentDetailsPage.js
@@ -2,17 +2,30 @@ import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { useParams } from "react-router-dom";
 import ApartmentTable from 'main/components/Apartments/ApartmentTable';
 import { apartmentUtils } from 'main/utils/apartmentUtils';
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
 
 export default function ApartmentDetailsPage() {
   let { id } = useParams();
 
-  const response = apartmentUtils.getById(id);
+
+  const { data: apartment, error, status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      [`/api/apartment?id=${id}`],
+      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+        method: "GET",
+        url: `/api/apartment`,
+        params: {
+          id
+        }
+      }
+    );
 
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Apartment Details</h1>
-        <ApartmentTable apartments={[response.apartment]} showButtons={false} />
+        {apartment && <ApartmentTable apartments={[apartment]} showButtons={false} />}
       </div>
     </BasicLayout>
   )
--- frontend/src/tests/pages/Apartments/ApartmentDetails.test.js
+++ frontend/src/tests/pages/Apartments/ApartmentDetails.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, queryByTestId, render, waitFor, screen } from "@testing-library/react";
 import ApartmentDetailsPage from "main/pages/Apartments/ApartmentDetailsPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
@@ -8,41 +8,38 @@ import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
+
 const mockNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
     useParams: () => ({
-        id: 3
+        id: 1
     }),
     useNavigate: () => mockNavigate
 }));
 
-jest.mock('main/utils/apartmentUtils', () => {
-    return {
-        __esModule: true,
-        apartmentUtils: {
-            getById: (_id) => {
-                return {
-                    apartment: {
-                        id: 3,
-                        name: "San Joaquin North Villages",
-                        address: "650 Storke Road",
-                        city: "Goleta",
-                        state: "CA",
-                        rooms: 166,
-                        description: "Nice",
-                    }
-                }
-            }
-        }
-    }
-});
 
 describe("ApartmentDetailsPage tests", () => {
 
-    const axiosMock =new AxiosMockAdapter(axios);
-    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither); 
+    const axiosMock = new AxiosMockAdapter(axios);
+
+    const testId = "ApartmentTable";
+
+    beforeEach(() => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+        axiosMock.onGet("/api/apartment", { params: { id: 1 } }).reply(200, {
+            "id": 1,
+            "name": "Sierra Madre Villages",
+            "address": "555 Storke Road",
+            "city": "Goleta",
+            "state": "CA",
+            "rooms": 109,
+            "description": "Nice and New"
+        });
+    });
 
     const queryClient = new QueryClient();
     test("renders without crashing", () => {
@@ -55,7 +52,71 @@ describe("ApartmentDetailsPage tests", () => {
         );
     });
 
-    test("loads the correct fields, and no buttons", async () => {
+    test("loads the correct fields", async () => {
+
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <ApartmentDetailsPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1"); });
+        expect(getByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("Sierra Madre Villages");
+        expect(getByTestId(`${testId}-cell-row-0-col-address`)).toHaveTextContent("555 Storke Road");
+        expect(getByTestId(`${testId}-cell-row-0-col-city`)).toHaveTextContent("Goleta");
+        expect(getByTestId(`${testId}-cell-row-0-col-state`)).toHaveTextContent("CA");
+        expect(getByTestId(`${testId}-cell-row-0-col-rooms`)).toHaveTextContent("109");
+        expect(getByTestId(`${testId}-cell-row-0-col-description`)).toHaveTextContent("Nice and New");
+    });
+
+    test("loads without buttons", async () => {
+
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <ApartmentDetailsPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(screen.queryByText("Delete")).not.toBeInTheDocument(); });
+        expect(screen.queryByText("Edit")).not.toBeInTheDocument();
+        expect(screen.queryByText("Details")).not.toBeInTheDocument();
+    });
+
+});
+
+/*
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+describe("ApartmentDetailsPage tests", () => {
+
+    const axiosMock =new AxiosMockAdapter(axios);
+
+    const testId = "ApartmentTable";
+
+    const setup = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
+
+    test("renders without crashing", () => {
+        setup();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/apartment/all").reply(200, []);
+
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -63,18 +124,72 @@ describe("ApartmentDetailsPage tests", () => {
                 </MemoryRouter>
             </QueryClientProvider>
         );
-        expect(screen.getByText("San Joaquin North Villages")).toBeInTheDocument();
-        expect(screen.getByText("650 Storke Road")).toBeInTheDocument();
-        expect(screen.getByText("Goleta")).toBeInTheDocument();
-        expect(screen.getByText("CA")).toBeInTheDocument();
-        expect(screen.getByText(166)).toBeInTheDocument();
-        expect(screen.getByText("Nice")).toBeInTheDocument();
-
-        expect(screen.queryByText("Delete")).not.toBeInTheDocument();
-        expect(screen.queryByText("Edit")).not.toBeInTheDocument();
-        expect(screen.queryByText("Details")).not.toBeInTheDocument();
     });
 
-});
+    test("renders apartment without crashing", async () => {
+        setup();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/apartment/all").reply(200, apartmentFixtures.oneApartment);
 
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <ApartmentDetailsPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
 
+        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1"); });
+        expect(getByTestId(`${testId}-cell-row-0-col-name`)).toHaveTextContent("Sierra Madre Villages");
+        expect(getByTestId(`${testId}-cell-row-0-col-address`)).toHaveTextContent("555 Storke Road");
+        expect(getByTestId(`${testId}-cell-row-0-col-city`)).toHaveTextContent("Goleta");
+        expect(getByTestId(`${testId}-cell-row-0-col-state`)).toHaveTextContent("CA");
+        expect(getByTestId(`${testId}-cell-row-0-col-rooms`)).toHaveTextContent("109");
+        expect(getByTestId(`${testId}-cell-row-0-col-description`)).toHaveTextContent("Nice and New");
+
+    });
+
+    test("renders without buttons", async () => {
+        setup();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/apartment/all").reply(200, apartmentFixtures.oneApartment);
+
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <ApartmentDetailsPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(screen.queryByText("Delete")).not.toBeInTheDocument(); });
+        expect(screen.queryByText("Edit")).not.toBeInTheDocument();
+        expect(screen.queryByText("Details")).not.toBeInTheDocument();
+
+    });
+
+    test("renders empty table when backend unavailable", async () => {
+        setup();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/apartment/all").timeout();
+
+        const restoreConsole = mockConsole();
+
+        const { queryByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <ApartmentDetailsPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1); });
+
+        const errorMessage = console.error.mock.calls[0][0];
+        expect(errorMessage).toMatch("Error communicating with backend via GET on /api/apartment");
+        restoreConsole();
+
+        expect(queryByTestId(`${testId}-cell-row-0-col-id`)).not.toBeInTheDocument();
+    });
+})
+*/

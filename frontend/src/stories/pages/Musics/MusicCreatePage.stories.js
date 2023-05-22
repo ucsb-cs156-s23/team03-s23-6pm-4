f7deb36b74aed96fe5941706c42900d5e5763694
Krish Agarwal <kshitij0407@gmail.com>
2023-05-22 12:42:57 -0700

    Added frontend edits - Krish

---
+++ frontend/src/stories/pages/Musics/MusicCreatePage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import MusicCreatePage from "main/pages/Musics/MusicCreatePage";
+
+export default {
+    title: 'pages/Musics/MusicCreatePage',
+    component: MusicCreatePage
+};
+
+const Template = () => <MusicCreatePage />;
+
+export const Default = Template.bind({});
+
+
+
+

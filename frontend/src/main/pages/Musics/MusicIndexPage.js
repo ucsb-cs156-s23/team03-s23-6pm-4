3b67398edd62c329de3ef1aae1190a120829ab19
Krish Agarwal <kshitij0407@gmail.com>
2023-05-22 12:42:57 -0700

    Added frontend edits - Krish

--- frontend/src/main/pages/Musics/MusicIndexPage.js
+++ frontend/src/main/pages/Musics/MusicIndexPage.js
@@ -1,34 +1,28 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
+import { useBackend } from 'main/utils/useBackend';
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import MusicTable from 'main/components/Musics/MusicTable';
-import { musicUtils } from 'main/utils/musicUtils';
-import { useNavigate, Link } from 'react-router-dom';
+import { useCurrentUser } from 'main/utils/currentUser'
 
 export default function MusicIndexPage() {
 
-    const navigate = useNavigate();
+  const currentUser = useCurrentUser();
 
-    const musicCollection = musicUtils.get();
-    const musics = musicCollection.musics;
+  const { data: musics, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/music/all"],
+      { method: "GET", url: "/api/music/all" },
+      []
+    );
 
-    const showCell = (cell) => JSON.stringify(cell.row.values);
-
-    const deleteCallback = async (cell) => {
-        console.log(`MusicIndexPage deleteCallback: ${showCell(cell)})`);
-        musicUtils.del(cell.row.values.id);
-        navigate("/musics");
-    }
-
-    return (
-        <BasicLayout>
-            <div className="pt-2">
-                <Button style={{ float: "right" }} as={Link} to="/musics/create">
-                    Create Music
-                </Button>
-                <h1>Musics</h1>
-                <MusicTable musics={musics} deleteCallback={deleteCallback} />
-            </div>
-        </BasicLayout>
-    )
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Musics</h1>
+        <MusicTable musics={musics} currentUser={currentUser} />
+      </div>
+    </BasicLayout>
+  )
 }
